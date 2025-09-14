9ff9de1ed867a165c4f83525b8adccdcdba4c668
raqibiddi <banksiddi@gmail.com>
2025-09-14 00:13:19 +0000

    Add initial basic python exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+#print(sum)
+difference = number1 - number2
+#print(difference)
+product = number1 * number2
+#print(product)
+
+
+print("Addition of", number1, "and", number2,  "is", sum)
+print("Subtraction of", number1, "and", number2,  "is", difference)
+print("Multiplication of", number1, "and", number2,  "is", product)
