ca7a73aff9fc8d864730815b73661fa826722b13
raqibiddi <banksiddi@gmail.com>
2025-09-21 23:20:32 +0000

    Change made to syntax of codebase

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,7 +2,7 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
     product = number * i
-    print(f"{number} x {i} = {product}", end="")
+    print(f"{number} * {i} = {product}", end="")
     print()
 
 
