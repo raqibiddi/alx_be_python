96b1f9b9f88c7d9bc6eee9e9895fd762639e72d7
raqibiddi <banksiddi@gmail.com>
2025-09-28 21:54:47 +0000

    Last fix to formatted date for future date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -12,7 +12,8 @@ number_of_days = int(input("Enter the number of days: "))
 
 def calculate_future_date():
     future_date = date.today() + timedelta(days=number_of_days)
-    return future_date
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    return formatted_future_date
 
 
 print(f"Future date: {calculate_future_date()}")
