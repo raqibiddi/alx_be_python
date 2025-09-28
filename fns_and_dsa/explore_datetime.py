2f997138c5f0b0dd9cce597f85bf1721ad5ef702
raqibiddi <banksiddi@gmail.com>
2025-09-28 21:57:09 +0000

    Syntax error fix in input from user

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -8,7 +8,7 @@ def display_current_datetime():
 
 display_current_datetime()
 
-number_of_days = int(input("Enter the number of days: "))
+number_of_days = int(input("Enter the number of days to add to the current date: "))
 
 def calculate_future_date():
     future_date = date.today() + timedelta(days=number_of_days)
